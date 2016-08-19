0e70b14d63606fa30377263cfc3d75b2a3518874
flakis05 <localadmin@compozed-tss-05.mobile.asu.edu>
2016-08-19 10:28:20 -0700

    added helpful echos

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 cd resource-smoke-test
+
+echo updating apt-get
+sudo apt-get update
+
+echo Intalling NPM
 sudo apt-get install npm
+
+echo npm install
 npm install
+
+echo running test
 mocha smoke_test.js
